6b79e749d0eddb2b1c7ba3e8ce12d73f285a0f20
gopala krishnan <128345773+Gopala230390@users.noreply.github.com>
2023-09-29 00:02:35 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -2,11 +2,10 @@ resource "aws_instance" "test-server" {
   ami           = "ami-0f5ee92e2d63afc18" 
   instance_type = "t2.micro" 
   key_name = "BabucKeypair"
-  vpc_security_group_ids= ["sg-00f7cae4b48423f8c"]
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("./BabucKeypair.pem")
+    private_key = file("./AWS-EC2-Key.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
